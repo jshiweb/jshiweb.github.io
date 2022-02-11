35ed2535e8d30e04615ad8c5525f02b96795b833
jessicashi <jessica.shi95@gmail.com>
2022-02-11 17:18:41 -0600

    hide

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,13 @@ import React from "react";
 import logo from "../logo.svg";
 
 function Home() {
+  // useEffect(() => {
+  //   console.log("running api call");
+  //   fetch("http://localhost:8000/todo/1")
+  //     .then((resp) => resp.json())
+  //     .then((resp1) => console.log(resp1["Data"]));
+  // });
+
   return (
     <div className="Body">
       <p>
